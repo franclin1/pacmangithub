f049b3197895ac1bf5017c4ed5af2fe5ccaf6640
Malte Ahlers <malteahlers@Maltes-MBP.fritz.box>
2022-12-20 16:17:40 +0100

    yy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ kubectl apply -f configmap.yaml
 kubectl apply -f deploynginx.yaml 
 kubectl apply -f mongodb.yaml 
 kubectl apply -f pacman.yaml 
-sleep 20
+kubectl rollout status deployment ingress-nginx-controller -n malteahlers
 kubectl apply -f ingress.yaml 
 sleep 5
 kubectl get all  -n malteahlers
